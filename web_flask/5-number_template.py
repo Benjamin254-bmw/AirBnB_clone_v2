f7c61892c15aeaf601a1c4e1bef8a0da1db51c46
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-03-11 22:49:59 +0300

    Number plate

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,4 +42,4 @@ def num_checker_template(n):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')#!/usr/bin/python3
+    app.run(debug=True, host='0.0.0.0', port='5000')
